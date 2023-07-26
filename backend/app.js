557fe81f574c021e68406db5756ab6c59d953715
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-07-26 16:35:38 +0100

    y

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ const displayRoute = require("./routes/display");
 const mongodbConnection = require('./config/mongoDBConnection');
 const port = 8000;
 
+app.use(cors());
 app.use("/api/display", displayRoute);
 
 app.listen(port, () => console.log(`Node JS server running on port ${port}!`));
