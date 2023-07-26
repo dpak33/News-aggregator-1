d0d09d4079917f492fb80a0407c63069121ed40f
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-07-26 16:24:59 +0100

    y

--- frontend/src/components/MainBar.jsx
+++ frontend/src/components/MainBar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const MainBar = () => {
-    return (
-        <div>MainBar</div>
-    )
-}
+    const [articles, setArticles] = useState([]);
 
-export default MainBar
+    useEffect(() => {
+        const fetchArticles = async () => {
+            try {
+                const res = await axios.get('http://localhost:8000/api/display/latestArticles'); // adjust the URL based on your backend setup
+                setArticles(res.data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        fetchArticles();
+    }, []); // Empty array as second argument to run this effect once on component mount
+
+    return (
+        <div>
+            {articles.map((article, index) => (
+                <div key={index}>
+                    <h2>{article.title}</h2>
+                    <img src={article.image} alt={article.title} />
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default MainBar;
