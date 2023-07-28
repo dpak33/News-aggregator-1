67a6dfd9716fea063ccf5df0048fabc57e939c77
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-07-28 12:18:21 +0100

    y

--- backend/routes/display.js
+++ backend/routes/display.js
@@ -5,7 +5,7 @@ const Article = require('../schemas/ArticleSchema');
 
 router.get('/latestArticles', async (req, res) => {
     try {
-        const articles = await Article.find().sort({ published: -1 }).limit(20);
+        const articles = await Article.find().sort({ published: -1 }).limit(25);
         res.json(articles);
     } catch (err) {
         res.status(500).json({ message: err.message });
