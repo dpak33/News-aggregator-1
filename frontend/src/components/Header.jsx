5bc9143cacf85a7ce810f4ea71d8785031fb892c
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-07-30 12:07:17 +0100

    y

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styling/Header.css';
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <header className="header">
-            <i className="close-icon material-symbols-outlined">
-                menu_open
+            <i
+                className="close-icon material-symbols-outlined"
+                onClick={() => {
+                    console.log("clicked!");
+                    setIsOpen(!isOpen)
+                }}
+            >menu_open
             </i>
+            {
+                isOpen && (
+                    <ul className="dropdown">
+                        <li>Most Recent</li>
+                        <li>Most Popular</li>
+                        <li>Your Saved Articles</li>
+                    </ul>
+                )
+            }
             <h1>News Aggregator</h1>
             <span className="production-icon">Pakenham Productions</span>
-        </header>
+        </header >
     )
 }
 
