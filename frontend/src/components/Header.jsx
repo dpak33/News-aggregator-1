b47308b5b3b5f134928e90d20522e8414886c198
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-07-30 16:44:01 +0100

    y

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,21 +6,21 @@ const Header = () => {
 
     return (
         <header className="header">
-            <div style={{ position: "relative" }}> {/* Add this wrapper */}
+            <div className="icon-wrapper"> {/* Wrap the icon with a div */}
                 <i
                     className="close-icon material-symbols-outlined"
                     onClick={() => setIsOpen(!isOpen)}
                 >
                     menu_open
                 </i>
-                {isOpen && (
-                    <ul className="dropdown">
-                        <li>Most Recent</li>
-                        <li>Most Popular</li>
-                        <li>Your Saved Articles</li>
-                    </ul>
-                )}
-            </div> {/* End wrapper */}
+            </div>
+            {isOpen && (
+                <ul className="dropdown">
+                    <li>Most Recent</li>
+                    <li>Most Popular</li>
+                    <li>Your Saved Articles</li>
+                </ul>
+            )}
             <h1>News Aggregator</h1>
             <span className="production-icon">Pakenham Productions</span>
         </header>
