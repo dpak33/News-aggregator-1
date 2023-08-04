0eb72c55f884ea6c648e20203d5eb3f9df8273b0
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-08-04 10:46:33 +0100

    y

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import toast from "react-hot-toast";
 import '../styling/Header.css';
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const navigate = useNavigate();
+
+    const isUserLoggedIn = () => {
+        return localStorage.getItem('user') !== null;
+    };
+
+    const logoutUser = () => {
+        localStorage.removeItem('user');
+        toast.success('User logged out');
+        navigate("/"); // or use the navigate function from 'react-router-dom' if you have it available
+    };
 
     return (
         <header className="header">
@@ -17,7 +30,15 @@ const Header = () => {
             </div>
             {isOpen && (
                 <ul className="dropdown">
-                    <li><Link to="/login">Login/signup</Link></li>
+                    {isUserLoggedIn() ? (
+                        <li onClick={logoutUser}>
+                            <a href="/">Logout</a> {/* You can replace this with a button or any other element */}
+                        </li>
+                    ) : (
+                        <li>
+                            <Link to="/login">Login/Signup</Link>
+                        </li>
+                    )}
                     <li>Most Recent</li>
                     <li>Most Popular</li>
                     <li>Your Saved Articles</li>
