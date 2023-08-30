e2d449ab922ee3c8489b5d4af425a4378c539982
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-08-30 09:54:21 +0100

    y

---
+++ backend/backendhelpers/transformations.js
@@ -0,0 +1,24 @@
+const transformGuardianArticle = (GuardianArticle) => {
+    return {
+        id: guardianArticle.id,
+        title: guardianArticle.fields.headline,
+        description: guardianArticle.fields.byline, // or whatever part of the bodyText you want
+        url: guardianArticle.webUrl,
+        author: guardianArticle.fields.byline,
+        image: guardianArticle.fields.thumbnail,
+        language: 'en',  // The Guardian is in English, you could map this dynamically if you want
+        category: guardianArticle.sectionName,
+        published: new Date(guardianArticle.webPublicationDate)
+    };
+};
+
+const transformNYTimesArticle = (NYTimesArticle) => {
+    return {
+
+    };
+}
+
+module.exports = {
+    transformNYTimesArticle,
+    transformGuardianArticle
+}
