bd4c45d858e7baeabb9fd6c4322c31dadfcc52ad
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-08-31 09:23:55 +0100

    y

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -12,9 +12,9 @@ mongoose.connect(process.env.MONGODB_URI, {
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Could not connect to MongoDB...', err));
 
-const currentsAPI = axios.get('https://api.currentsapi.services/v1/latest-news?apiKey=e7iyp7IB4DSu8-dI7GRpc2JeNz6SDjllgaNHu9bOzJrjobgO');
-const guardianAPI = axios.get('https://content.guardianapis.com/search?order-by=newest&show-fields=byline%2Cthumbnail%2Cheadline%2CbodyText&api-key=5d82019f-d864-4447-b34d-1ecf955ea762');
-const nyTimesAPI = axios.get('https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=OAChSwo5sGj7xEQRj8bQ9NLGBIHsM5iA');
+const currentsAPI = axios.get(`https://api.currentsapi.services/v1/latest-news?apiKey=${process.env.CURRENTS_API_KEY}`);
+const guardianAPI = axios.get(`https://content.guardianapis.com/search?order-by=newest&show-fields=byline%2Cthumbnail%2Cheadline%2CbodyText&api-key=${process.env.GUARDIAN_API_KEY}`);
+const nyTimesAPI = axios.get(`https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=${process.env.NYTIMES_API_KEY}`);
 
 Promise.all([currentsAPI, guardianAPI, nyTimesAPI])
     .then((responses) => {
