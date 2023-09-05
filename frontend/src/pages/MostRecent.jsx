176f4b797313fc5c9e7c8233cedc547d577315bc
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-09-05 09:42:58 +0100

    y

--- frontend/src/pages/MostRecent.jsx
+++ frontend/src/pages/MostRecent.jsx
@@ -3,7 +3,7 @@ import ArticleList from '../helpers/ArticleList';
 
 const MostRecent = () => {
 
-    const route = "http://localhost:8000/api/display/latestArticles";
+    const route = "http://localhost:8000/api/display/latestArticlespage";
     return <ArticleList route={route} pageTitle="Most Recent" />;
 }
 
