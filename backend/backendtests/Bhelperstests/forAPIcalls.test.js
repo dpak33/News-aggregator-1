aa5693c0537aec8ea19324996ab906a0008f0617
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-09-16 09:39:10 +0100

    y

--- backend/backendtests/Bhelperstests/forAPIcalls.test.js
+++ backend/backendtests/Bhelperstests/forAPIcalls.test.js
@@ -13,4 +13,51 @@ describe('fetchArticles', () => {
         expect(Array.isArray(articles)).toBe(true);
         expect(axios.get).toHaveBeenCalledTimes(3);
     });
+
+    it('should fetch articles from Currents API correctly', async () => {
+        await fetchArticles();
+
+        // Assertions related to the Currents API, for example:
+        expect(axios.get).toHaveBeenCalledWith(expect.stringContaining('currentsapi'));
+    });
+
+    it('should deal with the correct response object structure for each API call', async () => {
+        const mockCurrentsResponse = { data: { news: ['currentsArticle1', 'currentsArticle2'] } };
+        const mockNYTimesResponse = { data: { results: ['nyTimesArticle1', 'nyTimesArticle2'] } };
+        const mockGuardianResponse = { data: { response: { results: ['guardianArticle1', 'guardianArticle2'] } } };
+
+        axios.get.mockImplementation((url) => {
+            if (url.includes('currentsapi')) return Promise.resolve(mockCurrentsResponse);
+            if (url.includes('nytimes')) return Promise.resolve(mockNYTimesResponse);
+            if (url.includes('guardianapis')) return Promise.resolve(mockGuardianResponse);
+            return Promise.reject(new Error('URL not recognized'));
+        });
+
+        await fetchArticles(); // call fetchArticles to trigger the axios.get mock.
+
+        expect(axios.get).toHaveBeenCalledWith(expect.stringContaining('currentsapi'));
+        expect(axios.get).toHaveBeenCalledWith(expect.stringContaining('nytimes'));
+        expect(axios.get).toHaveBeenCalledWith(expect.stringContaining('guardianapis'));
+    });
+
+    it('should correctly combine and transform articles from all APIs', async () => {
+        const mockCurrentsResponse = { data: { news: ['currentsArticle1', 'currentsArticle2'] } };
+        const mockNYTimesResponse = { data: { results: ['nyTimesArticle1', 'nyTimesArticle2'] } };
+        const mockGuardianResponse = { data: { response: { results: ['guardianArticle1', 'guardianArticle2'] } } };
+
+        axios.get.mockImplementation((url) => {
+            if (url.includes('currentsapi')) return Promise.resolve(mockCurrentsResponse);
+            if (url.includes('nytimes')) return Promise.resolve(mockNYTimesResponse);
+            if (url.includes('guardianapis')) return Promise.resolve(mockGuardianResponse);
+            return Promise.reject(new Error('URL not recognized'));
+        });
+
+        const articles = await fetchArticles();
+
+        // Check if all articles are in the response
+        expect(articles).toEqual(expect.arrayContaining(['currentsArticle1', 'nyTimesArticle1', 'guardianArticle1']));
+        expect(articles).toEqual(expect.arrayContaining(['currentsArticle2', 'nyTimesArticle2', 'guardianArticle2']));
+
+    });
 });
+
