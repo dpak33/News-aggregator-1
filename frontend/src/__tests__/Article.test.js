86af6867017dcc3d401501602d0a0865ad41be2d
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-09-27 10:46:08 +0100

    y

--- frontend/src/__tests__/Article.test.js
+++ frontend/src/__tests__/Article.test.js
@@ -1,81 +1,128 @@
 import React from 'react';
-import { render, screen, waitFor, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import axios from 'axios';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import mockAxios from 'jest-mock-axios';
 import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
 import Article from '../components/Article';
 
-const mockStore = configureMockStore([thunk]);
 
-describe('Article component', () => {
+jest.mock('axios');
+jest.mock('react-hot-toast');
+
+describe('Article Component', () => {
+    const mockStore = configureMockStore();
+    const article = {
+        id: '1',
+        title: 'Test Article',
+        description: 'Test Description',
+        url: 'http://testurl.com',
+        image: 'http://testimage.com',
+        likes: [],
+        likesCount: 0,
+    };
+
+    const userId = '123';
 
     let store;
 
     beforeEach(() => {
         store = mockStore({
             user: {
-                userId: 'testUserId',
+                userId,
                 savedArticles: []
             }
         });
+
+        store.dispatch = jest.fn();
+
+        axios.post.mockResolvedValue({});
     });
 
     afterEach(() => {
-        mockAxios.reset();
+        jest.clearAllMocks();
     });
 
-    it('renders article data correctly', () => {
-        const mockArticle = {
-            id: '123',
-            image: 'test-image.jpg',
-            title: 'Test Title',
-            description: 'Test description',
-            url: 'http://testurl.com',
-            likesCount: 10,
-            likes: []
-        };
-
-        render(
+    it('renders the initial elements correctly', () => {
+        const { getByText, getByRole } = render(
             <Provider store={store}>
-                <Article article={mockArticle} />
+                <Article article={article} />
             </Provider>
         );
 
-        expect(screen.getByText('Test Title')).toBeInTheDocument();
-        expect(screen.getByText('Test description')).toBeInTheDocument();
-        expect(screen.getByRole('img')).toHaveAttribute('src', 'test-image.jpg');
+        expect(getByText('Test Article')).toBeInTheDocument();
+        expect(getByText('Test Description')).toBeInTheDocument();
+        expect(getByRole('img')).toHaveAttribute('src', 'http://testimage.com');
+        expect(getByText('Like')).toBeInTheDocument();
+        expect(getByText('Save')).toBeInTheDocument();
     });
 
-    it('handles like toggle correctly', async () => {
-        const mockArticle = {
-            id: '123',
-            likes: [],
-            //... other properties
-        };
-
-        render(
+    it('renders correctly and likes/unlikes article', async () => {
+        const { getByText, getByRole } = render(
             <Provider store={store}>
-                <Article article={mockArticle} />
+                <Article article={article} />
             </Provider>
         );
 
-        fireEvent.click(screen.getByText('Like'));
+        // Assert initial render
+        expect(getByText('Test Article')).toBeInTheDocument();
+        expect(getByText('Test Description')).toBeInTheDocument();
+        expect(getByRole('img')).toHaveAttribute('src', 'http://testimage.com');
+        expect(getByText('Like')).toBeInTheDocument();
+        expect(getByText('Save')).toBeInTheDocument();
+
+        // Like the article
+        fireEvent.click(getByText('Like'));
 
         await waitFor(() => {
-            // Check if axios was called correctly
-            expect(mockAxios.post).toHaveBeenCalledWith(
-                `http://localhost:8000/api/likes/like/${encodeURIComponent(mockArticle.id)}`,
-                { userId: 'testUserId' },
-                {
-                    headers: {
-                        'Authorization': `Bearer ${localStorage.getItem('user')}`
-                    }
-                }
-            );
+            expect(axios.post).toHaveBeenCalledWith('http://localhost:8000/api/likes/like/1', { userId }, expect.any(Object));
+            expect(store.dispatch).toHaveBeenCalledWith(expect.objectContaining({ type: 'LIKE_ARTICLE', payload: article.id }));
         });
+
+        // Assert Like state and action
+        expect(getByText('Unlike')).toBeInTheDocument();
+
+        // Unlike the article
+        fireEvent.click(getByText('Unlike'));
+
+        await waitFor(() => {
+            expect(axios.post).toHaveBeenCalledWith('http://localhost:8000/api/likes/unlike/1', { userId }, expect.any(Object));
+            expect(store.dispatch).toHaveBeenCalledWith(expect.objectContaining({ type: 'UNLIKE_ARTICLE', payload: article.id }));
+        });
+
+        // Assert Unlike state and action
+        expect(getByText('Like')).toBeInTheDocument();
     });
 
-    // Similarly, you can add more tests for unliking, saving, and unsaving behaviors.
+    it('saves and unsaves the article', async () => {
+        const { getByText } = render(
+            <Provider store={store}>
+                <Article article={article} />
+            </Provider>
+        );
+
+        // Assert initial render
+        expect(getByText('Save')).toBeInTheDocument();
+
+        // Save the article
+        fireEvent.click(getByText('Save'));
+
+        await waitFor(() => {
+            expect(axios.post).toHaveBeenCalledWith('http://localhost:8000/api/saves/save/1', { userId }, expect.any(Object));
+            expect(store.dispatch).toHaveBeenCalledWith(expect.objectContaining({ type: 'SAVE_ARTICLE', payload: article.id }));
+        });
+
+        // Assert Save state and action
+        expect(getByText('Unsave')).toBeInTheDocument();
+
+        // Unsave the article
+        fireEvent.click(getByText('Unsave'));
+
+        await waitFor(() => {
+            expect(axios.post).toHaveBeenCalledWith('http://localhost:8000/api/saves/unsave/1', { userId }, expect.any(Object));
+            expect(store.dispatch).toHaveBeenCalledWith(expect.objectContaining({ type: 'UNSAVE_ARTICLE', payload: article.id }));
+        });
+
+        // Assert Unsave state and action
+        expect(getByText('Save')).toBeInTheDocument();
+    });
 });
