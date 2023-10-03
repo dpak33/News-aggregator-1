d47f826e29573acd9150ac21d6f3fbee0047e99e
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-10-03 10:18:12 +0100

    y

--- backend/fetchAPIs/fetchNY.js
+++ backend/fetchAPIs/fetchNY.js
@@ -5,9 +5,9 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 async function fetchNY() {
     try {
         const response = await axios.get(`https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=${process.env.NYTIMES_API_KEY}`);
-        console.log("Guardian API Response:", JSON.stringify(response.data, null, 2));
+        console.log("NY Times API Response:", JSON.stringify(response.data, null, 2));
     } catch (error) {
-        console.error("Error fetching Guardian articles:", error);
+        console.error("Error fetching NY times articles:", error);
     }
 }
 
