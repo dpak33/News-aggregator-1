42a4c10ccf4ecd8f54e95996db87c4905fb53a16
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-10-06 14:15:59 +0100

    y

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // Catch-all route
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontned/build/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Node JS server running on port ${port}!`));
