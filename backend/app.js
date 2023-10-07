c8ea84380b57902fc11c0f2617decb1f4952cdab
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-10-07 12:42:12 +0100

    y

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,13 @@ app.use("/api/likes", jwtMiddleWare, likesRoute);
 app.use("/api/saves", jwtMiddleWare, savesRoute);
 app.use("/api/articles", articleRoute);
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Catch-all route should be the last one
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
 
 app.listen(port, () => console.log(`Node JS server running on port ${port}!`));
 
